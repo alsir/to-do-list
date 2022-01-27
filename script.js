f2d5b1409458288c41e0c814034aec2a6f9c9569
alsir <41924833+alsir@users.noreply.github.com>
2022-01-27 02:46:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ var num = $(".to-dos").length;
 $("#toggole-button").click(
   function(){
   	$("#toggole-button").toggle(function(){
-  		var elementVar = document.getElementById("#toggole-button");
-        elementVar.setAttribute("scr", "./images/icon-sun.svg");
+  		$("#toggole-button").attr("scr","./images/icon-sun.svg");
   		$(".head").css({"background-image":" url(./images/bg-mobile-dark.jpg)"});
   		$("body").css({"background-color":" hsl(235, 21%, 11%)"});
   		$(".to-do-cr").css({"background-color":"hsl(235, 24%, 19%)"});
@@ -12,6 +11,5 @@ $("#toggole-button").click(
   		$(".items").css({"background-color":"hsl(235, 24%, 19%)"});
   	}
   	);
-  	$("#toggole-button").attr("scr","./images/icon-sun.svg" );
   }
 	);
